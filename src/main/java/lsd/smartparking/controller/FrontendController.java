7b2d6f81e61d6db30cfe9229b68e57b39b78f174
Luca Ruschioni <Ruschio@users.noreply.github.com>
2020-11-09 12:43:59 +0100

    Add Vue.js on frontend part

---
+++ src/main/java/lsd/smartparking/controller/FrontendController.java
@@ -0,0 +1,17 @@
+package lsd.smartparking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+public class FrontendController {
+
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    // PROBLEM: chrome doesn't detect pwa
+    /*@RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirectFrontend() {
+        return "forward:/";
+    }*/
+    
+}

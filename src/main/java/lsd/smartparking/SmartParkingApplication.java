3183c3cd8de7abe4a61c3d6452f8517d995060b5
Luca Ruschioni <Ruschio@users.noreply.github.com>
2020-11-11 17:15:55 +0100

    Add Firebase sdk for backend

--- src/main/java/lsd/smartparking/SmartParkingApplication.java
+++ src/main/java/lsd/smartparking/SmartParkingApplication.java
@@ -1,12 +1,36 @@
 package lsd.smartparking;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @SpringBootApplication
 public class SmartParkingApplication {
 
 	public static void main(String[] args) {
+		try {
+			HashMap<String, Object> admin = new HashMap<String, Object>();
+			admin.put("uid", "crime-admin-server");
+			
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("static/resources/file/serviceAccountKey.json").getInputStream()))
+                .setDatabaseUrl("https://crimescraper.firebaseio.com")
+                .setDatabaseAuthVariableOverride(admin)
+                .build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
 		SpringApplication.run(SmartParkingApplication.class, args);
 	}
 

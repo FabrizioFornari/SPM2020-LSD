2eab0ab3f0d6de19a42bb74c6914e73a9880e01f
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-11-12 18:27:20 +0100

    Added 3 different types of registration form

--- src/frontend/src/views/Register.vue
+++ src/frontend/src/views/Register.vue
@@ -4,51 +4,138 @@
       <div class="col-md-8">
         <div class="card">
           <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{error}}</div>
-            <form @submit.prevent="signIn">
+			Registrati come
+			<select id="mySelect" @change="myFunction">
+				<option value="0">Utente</option>
+				<option value="1">Agente di Polizia</option>
+				<option value="2">Comune</option>
+			</select>
+			<br><br>
+            <form id ="form1">
             <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
+                <label for="nameUser" class="col-md-4 col-form-label text-md-right">Nome</label>
                 <div class="col-md-6">
-                  <input id="name" type="text" class="form-control" name="name" value required autofocus
-                    v-model="form.name"/>
+                  <input id="nameUser" type="text" class="form-control" name="nameUser" value required autofocus
+                    v-model="form.nameUser"/>
                 </div>
               </div>
               <div class="form-group row">
-                <label for="surname" class="col-md-4 col-form-label text-md-right">Cognome</label>
+                <label for="surnameUser" class="col-md-4 col-form-label text-md-right">Cognome</label>
                 <div class="col-md-6">
-                  <input id="surname" type="text" class="form-control" name="surname" value required autofocus
-                    v-model="form.surname"/>
+                  <input id="surnameUser" type="text" class="form-control" name="surnameUser" value required autofocus
+                    v-model="form.surnameUser"/>
                 </div>
               </div>
               <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+                <label for="emailUser" class="col-md-4 col-form-label text-md-right">Email</label>
                 <div class="col-md-6">
-                  <input id="email" type="email" class="form-control" name="email" value required autofocus
-                    v-model="form.email"/>
+                  <input id="emailUser" type="email" class="form-control" name="emailUser" value required autofocus
+                    v-model="form.emailUser"/>
                 </div>
               </div>
               <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                <label for="passwordUser" class="col-md-4 col-form-label text-md-right">Password</label>
                 <div class="col-md-6">
-                  <input id="password" type="password" class="form-control" name="password" required
-                    v-model="form.password"/>
+                  <input id="passwordUser" type="password" class="form-control" name="passwordUser" required
+                    v-model="form.passwordUser"/>
                 </div>
               </div>
               <div class="form-group row">
-                <label for="passwordConfirm" class="col-md-4 col-form-label text-md-right">Conferma password</label>
+                <label for="passwordUserConfirm" class="col-md-4 col-form-label text-md-right">Conferma password</label>
                 <div class="col-md-6">
-                  <input id="passwordConfirm" type="password" class="form-control" name="passwordConfirm" required
-                    v-model="form.passwordConfirm"/>
+                  <input id="passwordUserConfirm" type="password" class="form-control" name="passwordUserConfirm" required
+                    v-model="form.passwordUserConfirm"/>
                 </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Registrati</button>
-                </div>
-                <p><a href="/login">Sei già registrato?</a></p>
               </div>
             </form>
+            <form id ="form2" style="display:none">
+            <div class="form-group row">
+                <label for="namePolice" class="col-md-4 col-form-label text-md-right">Nome</label>
+                <div class="col-md-6">
+                  <input id="namePolice" type="text" class="form-control" name="namePolice" value required autofocus
+                    v-model="form.namePolice"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="surnamePolice" class="col-md-4 col-form-label text-md-right">Cognome</label>
+                <div class="col-md-6">
+                  <input id="surnamePolice" type="text" class="form-control" name="surnamePolice" value required autofocus
+                    v-model="form.surnamePolice"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="districtPolice" class="col-md-4 col-form-label text-md-right">Dipartimento</label>
+                <div class="col-md-6">
+                  <input id="districtPolice" type="text" class="form-control" name="districtPolice" value required autofocus
+                    v-model="form.districtPolice"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="emailPolice" class="col-md-4 col-form-label text-md-right">Email</label>
+                <div class="col-md-6">
+                  <input id="emailPolice" type="email" class="form-control" name="emailPolice" value required autofocus
+                    v-model="form.emailPolice"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="passwordPolice" class="col-md-4 col-form-label text-md-right">Password</label>
+                <div class="col-md-6">
+                  <input id="passwordPolice" type="password" class="form-control" name="passwordPolice" required
+                    v-model="form.passwordPolice"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="passwordPoliceConfirm" class="col-md-4 col-form-label text-md-right">Conferma password</label>
+                <div class="col-md-6">
+                  <input id="passwordPoliceConfirm" type="password" class="form-control" name="passwordPoliceConfirm" required
+                    v-model="form.passwordPoliceConfirm"/>
+                </div>
+              </div>
+            </form>
+            <form id ="form3" style="display:none">
+            <div class="form-group row">
+                <label for="nameMunicipality" class="col-md-4 col-form-label text-md-right">Nome</label>
+                <div class="col-md-6">
+                  <input id="nameMunicipality" type="text" class="form-control" name="nameMunicipality" value required autofocus
+                    v-model="form.nameMunicipality"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="districtMunicipality" class="col-md-4 col-form-label text-md-right">Città</label>
+                <div class="col-md-6">
+                  <input id="districtMunicipality" type="text" class="form-control" name="districtMunicipality" value required autofocus
+                    v-model="form.districtMunicipality"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="emailMunicipality" class="col-md-4 col-form-label text-md-right">Email</label>
+                <div class="col-md-6">
+                  <input id="emailMunicipality" type="email" class="form-control" name="emailMunicipality" value required autofocus
+                    v-model="form.emailMunicipality"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="passwordMunicipality" class="col-md-4 col-form-label text-md-right">Password</label>
+                <div class="col-md-6">
+                  <input id="passwordMunicipality" type="password" class="form-control" name="passwordMunicipality" required
+                    v-model="form.passwordMunicipality"/>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="passwordMunicipalityConfirm" class="col-md-4 col-form-label text-md-right">Conferma password</label>
+                <div class="col-md-6">
+                  <input id="passwordConfirmMunicipality" type="password" class="form-control" name="passwordMunicipalityConfirm" required
+                    v-model="form.passwordConfirmMunicipality"/>
+                </div>
+              </div>
+            </form>
+            <div class="form-group row mb-0">
+              <div class="col-md-12">
+                <button type="submit" class="btn btn-primary">Registrati</button>
+                <br><br>
+                <p><a href="/login">Sei già registrato?</a></p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -57,6 +144,7 @@
 </template>
 
 <script>
+	
 import { login } from '@/firebase';
 
 export default {
@@ -65,8 +153,7 @@ export default {
       form: {
         email: "",
         password: ""
-      },
-      error: null
+      }
     };
   },
   methods: {
@@ -75,7 +162,16 @@ export default {
       this.$router.push(this.$route.query.redirect || '/')
       this.form.email = ""
       this.form.password = ""
-    }
+    },
+    myFunction() {
+		document.getElementById("form1").style.display="none";
+		document.getElementById("form2").style.display="none";
+		document.getElementById("form3").style.display="none";
+		const x = document.getElementById("mySelect").value;
+		if (x === "0") document.getElementById("form1").style.display="block";
+		else if (x === "1") document.getElementById("form2").style.display="block";
+		else if (x === "2") document.getElementById("form3").style.display="block";
+	}
   }
 };
 </script>

4cd39d8fde5d79e833eaed999d20c94389bc6192
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-11-13 16:05:41 +0100

    Switch realtime db functions to cloud firestore

--- src/main/java/lsd/smartparking/controller/AuthController.java
+++ src/main/java/lsd/smartparking/controller/AuthController.java
@@ -1,73 +1,43 @@
 package lsd.smartparking.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.context.request.async.DeferredResult;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.cloud.FirestoreClient;
 
 import lsd.smartparking.model.Municipality;
 import lsd.smartparking.model.Policeman;
-import lsd.smartparking.model.User;
 
 
 @Controller
 public class AuthController {
 	
-	private DatabaseReference policemanRef = FirebaseDatabase.getInstance().getReference("Policemen");
-	private DatabaseReference municipalityRef = FirebaseDatabase.getInstance().getReference("Municipality");
+	Firestore db = FirestoreClient.getFirestore();
+	DocumentReference policemanRef = db.collection("Policemen").document();
+	DocumentReference municipalityRef = db.collection("Municipality").document();
+
 	
 	@PostMapping("register/user/{uid}/{name}/{surname}/{email}/{municipalityId}")
-	public DeferredResult<String> newPoliceman(@PathVariable("uid") String uid, @PathVariable("name") String name, 
+	public String newPoliceman(@PathVariable("uid") String uid, @PathVariable("name") String name, 
 						   @PathVariable("surname") String surname, @PathVariable("email") String email,
 						   @PathVariable("municipalityId") String municipalityId) throws IOException, FirebaseAuthException {
-		DeferredResult<String> result = new DeferredResult<>();
 		Policeman p = new Policeman(name, surname, email, uid, municipalityId);
-		 
-		policemanRef.child(uid).setValue(p, new DatabaseReference.CompletionListener() {
-			@Override
-			public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-				if (databaseError != null) {
-					System.out.println("Data could not be saved " + databaseError.getMessage());
-				} else {
-					//TODO set Result for frontend
-				}
-			}
-		});
-        return result;
+		policemanRef.set(p);
+		return "";
 	}
 	
 	@PostMapping("register/user/{uid}/{city}/{province}/{region}/{email}")
-	public DeferredResult<String> newMunicipality(@PathVariable("uid") String uid, @PathVariable("city") String city, 
+	public String newMunicipality(@PathVariable("uid") String uid, @PathVariable("city") String city, 
 						   @PathVariable("province") String province, @PathVariable("region") String region,
 						   @PathVariable("email") String email) throws IOException, FirebaseAuthException {
-		DeferredResult<String> result = new DeferredResult<>();
 		Municipality m = new Municipality(city, province, region, uid, email);
-		 
-		municipalityRef.child(uid).setValue(m, new DatabaseReference.CompletionListener() {
-			@Override
-			public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-				if (databaseError != null) {
-					System.out.println("Data could not be saved " + databaseError.getMessage());
-				} else {
-					//TODO set Result for frontend
-				}
-			}
-		});
-        return result;
+		municipalityRef.set(m);
+        return "";
 	}
 }

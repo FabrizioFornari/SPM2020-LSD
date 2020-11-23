64b2bf458e3c4c65a883612f6463d3cb1e94c31b
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-11-23 10:46:14 +0100

    Implement user fields checking

--- src/main/java/lsd/smartparking/model/User.java
+++ src/main/java/lsd/smartparking/model/User.java
@@ -11,10 +11,10 @@ public class User {
     public User() { }
 
 	public User(String name, String surname, String email, String id) {
-		this.id = id;
-		this.name = name;
-		this.surname = surname;
-		this.email = email;
+		this.setName(name);
+		this.setSurname(surname);
+		this.setEmail(email);
+		this.setId(id);
 	}
 
 	public String getId() {
@@ -22,7 +22,8 @@ public class User {
 	}
 
 	public void setId(String id) {
-		this.id = id;
+		checkFields(id);
+		this.id = id.trim();
 	}	
 
 	public String getName() {
@@ -30,7 +31,11 @@ public class User {
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		checkFields(name);
+		if (name.length() > 20) {
+			throw new IllegalArgumentException("The name is too long");
+		}
+		this.name = name.trim();
 	}
 
 	public String getSurname() {
@@ -38,7 +43,11 @@ public class User {
 	}
 
 	public void setSurname(String surname) {
-		this.surname = surname;
+		checkFields(surname);
+		if (surname.length() > 20) {
+			throw new IllegalArgumentException("The surname is too long");
+		}
+		this.surname = surname.trim();
 	}
 
 	public String getEmail() {
@@ -46,7 +55,17 @@ public class User {
 	}
 
 	public void setEmail(String email) {
-		this.email = email;
+		checkFields(email);
+		if (email.length() > 20) {
+			throw new IllegalArgumentException("The email is too long");
+		}
+		this.email = email.trim();
+	}
+	
+	private void checkFields(String field) {
+		if (field == null || field == "") {
+			throw new IllegalArgumentException("The field cannot be null or empty");
+		}
 	}
 	
 }

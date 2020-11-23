200eb9999d8bb078bd54d57e82706de328b138d4
Luca Ruschioni <Ruschio@users.noreply.github.com>
2020-11-23 17:48:53 +0100

    Add Firebase config

--- src/main/java/lsd/smartparking/SmartParkingApplication.java
+++ src/main/java/lsd/smartparking/SmartParkingApplication.java
@@ -1,38 +1,12 @@
 package lsd.smartparking;
 
-import java.io.IOException;
-import java.util.HashMap;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 @SpringBootApplication
 public class SmartParkingApplication {
 
-	public static void main(String[] args) {
-		try {
-			HashMap<String, Object> admin = new HashMap<String, Object>();
-			admin.put("uid", "crime-admin-server");
-			
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("static/resources/file/serviceAccountKey.json").getInputStream()))
-                .setDatabaseUrl("https://sparking-uni.firebaseio.com/")
-                .setDatabaseAuthVariableOverride(admin)
-                .build();
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-		
+	public static void main(String[] args) {		
 		SpringApplication.run(SmartParkingApplication.class, args);
 	}
 
---
+++ src/main/java/lsd/smartparking/configuration/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package lsd.smartparking.configuration;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        HashMap<String, Object> admin = new HashMap<String, Object>();
+        admin.put("uid", "crime-admin-server");
+        
+        FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("static/resources/file/serviceAccountKey.json").getInputStream()))
+            .setDatabaseUrl("https://sparking-uni.firebaseio.com/")
+            .setDatabaseAuthVariableOverride(admin)
+            .build();
+            
+        FirebaseApp.initializeApp(options);
+    }
+
+}

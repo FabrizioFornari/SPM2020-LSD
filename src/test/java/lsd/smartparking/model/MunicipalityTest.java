6a3ece29f346c91d2c76051a53888f037692ce89
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-11-23 19:07:40 +0100

    Municipality class tests

---
+++ src/test/java/lsd/smartparking/model/MunicipalityTest.java
@@ -0,0 +1,81 @@
+package lsd.smartparking.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {Municipality.class})
+public class MunicipalityTest {
+
+    private Municipality municipality;
+	private String testCity;
+	private String testProvince;
+	private String testRegion;
+	private String testEmail;
+	private String testId;
+	
+	@BeforeEach
+	public void createMunicipality() {
+		testCity = "Castelfidardo";
+		testProvince = "Ancona";
+		testRegion = "Marche";
+		testEmail = "a@a.it";
+		testId = UUID.randomUUID().toString();
+		municipality = new Municipality(testCity, testProvince, testRegion, testEmail, testId);
+	}
+	
+	@Test
+	public void checkMunicipalityFields() {
+		assertEquals(municipality.getClass(), Municipality.class);
+		assertEquals(municipality.getCity(), testCity);
+		assertEquals(municipality.getProvince(), testProvince);
+		assertEquals(municipality.getRegion(), testRegion);
+		assertEquals(municipality.getEmail(), testEmail);
+		assertEquals(municipality.getId(), testId);
+	}
+	
+	@Test
+	public void isMunicipalityApproved() {
+		assertEquals(municipality.isApproved(), false);
+	}
+	
+	@Test
+	public void createMunicipalityInvalidCity() {
+		assertThrows(IllegalArgumentException.class, () -> municipality.setCity(null));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setCity(""));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setCity("123456789012345678901"));
+	}
+	
+	@Test
+	public void createMunicipalityInvalidProvince() {
+		assertThrows(IllegalArgumentException.class, () -> municipality.setProvince(null));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setProvince(""));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setProvince("123456789012345678901"));
+	}
+	
+	@Test
+	public void createMunicipalityInvalidRegion() {
+		assertThrows(IllegalArgumentException.class, () -> municipality.setRegion(null));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setRegion(""));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setRegion("123456789012345678901"));
+	}
+	
+	@Test
+	public void createMunicipalityInvalidEmail() {
+		assertThrows(IllegalArgumentException.class, () -> municipality.setEmail(null));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setEmail(""));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setEmail("123456789012345678901"));
+	}
+	
+	@Test
+	public void createMunicipalityInvalidId() {
+		assertThrows(IllegalArgumentException.class, () -> municipality.setId(null));
+		assertThrows(IllegalArgumentException.class, () -> municipality.setId(""));
+	}
+	
+}

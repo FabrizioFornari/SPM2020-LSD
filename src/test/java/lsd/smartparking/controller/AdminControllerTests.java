b6746609d64a9d75a461f8d4085fd6cd48ddf3e3
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-11-25 09:51:16 +0100

    AdminController test (problem with firebase), not valid test

---
+++ src/test/java/lsd/smartparking/controller/AdminControllerTests.java
@@ -0,0 +1,14 @@
+package lsd.smartparking.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {AdminController.class})
+public class AdminControllerTests {
+
+	@Test
+	void contextLoads() {
+		// TODO
+	}
+	
+}

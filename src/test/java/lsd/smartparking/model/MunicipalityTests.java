3067ecf5514ad7d920f2a867bae52efe63ada2ea
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-11-25 08:48:00 +0100

    rename municipality test

--- src/test/java/lsd/smartparking/model/MunicipalityTests.java
+++ src/test/java/lsd/smartparking/model/MunicipalityTests.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = {Municipality.class})
-public class MunicipalityTest {
+public class MunicipalityTests {
 
     private Municipality municipality;
 	private String testCity;

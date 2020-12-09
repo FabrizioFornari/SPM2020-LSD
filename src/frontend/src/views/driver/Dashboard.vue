de9eb926591dafdf120c0fd5cec137d46b08f8a2
Luca Ruschioni <Ruschio@users.noreply.github.com>
2020-12-09 19:01:08 +0100

    Move driver views to subfolder

--- src/frontend/src/views/driver/Dashboard.vue
+++ src/frontend/src/views/driver/Dashboard.vue
@@ -3,12 +3,16 @@
     <h1><b-badge variant="success">Hi {{ name }}!</b-badge></h1>
     <h5>This is your dashboard</h5>
     <button @click="signOut()">Logout</button>
+
+    <car-list />
   </div>
 </template>
 
 <script>
 import store from '@/store/auth'
 import { logout } from '@/firebase'
+import CarList from '@/components/CarList.vue'
+import CarList from './CarList.vue'
 
 export default {
   name: 'dashboard',
@@ -17,11 +21,13 @@ export default {
       name: store.getters.user.name,
     }
   },
+  components: {
+    CarList
+  },
   methods: {
     signOut() {
       logout();
     }
   }
 }
-
 </script>

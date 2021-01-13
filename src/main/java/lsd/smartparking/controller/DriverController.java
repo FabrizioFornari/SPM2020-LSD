2432333657239c0df670b10b3a1b83e2433b10b2
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2021-01-13 18:56:53 +0100

    Add ticket function

--- src/main/java/lsd/smartparking/controller/DriverController.java
+++ src/main/java/lsd/smartparking/controller/DriverController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lsd.smartparking.model.Ticket;
 import lsd.smartparking.model.Vehicle;
 
 @RestController()
@@ -24,6 +25,7 @@ public class DriverController extends TokenChecker {
     private final String role = "driver";
 	Firestore db = FirestoreClient.getFirestore();
     CollectionReference vehicleRef = db.collection("Vehicles");
+    CollectionReference ticketRef = db.collection("Tickets");
 
     
     @PostMapping("add/vehicle/{driverId}/{token}")
@@ -31,6 +33,16 @@ public class DriverController extends TokenChecker {
     		@RequestBody Vehicle vehicle) throws FirebaseAuthException {
         if (checkToken(driverId, token, role)) {
     		ApiFuture<DocumentReference> addedDocRef = vehicleRef.add(vehicle);
+	        return (new Gson().toJson(addedDocRef));
+        }
+        return "Error";
+    };
+    
+    @PostMapping("buy/ticket/{driverId}/{token}")
+    public @ResponseBody String buyTicket(@PathVariable("driverId") String driverId, @PathVariable("token") String token,
+    		@RequestBody Ticket ticket) throws FirebaseAuthException {
+        if (checkToken(driverId, token, role)) {
+    		ApiFuture<DocumentReference> addedDocRef = ticketRef.add(ticket);
 	        return (new Gson().toJson(addedDocRef));
         }
         return "Error";

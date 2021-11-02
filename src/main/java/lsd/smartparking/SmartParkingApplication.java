87f477985509593c015866d7a4e0d8f878ad771b
Luca Ruschioni <Ruschio@users.noreply.github.com>
2021-11-02 18:45:20 +0100

    Add component scan on main class

--- src/main/java/lsd/smartparking/SmartParkingApplication.java
+++ src/main/java/lsd/smartparking/SmartParkingApplication.java
@@ -3,9 +3,15 @@ package lsd.smartparking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EntityScan("lsd.smartparking*")
+@ComponentScan("lsd.smartparking*")
+@EnableMongoRepositories(basePackages="lsd.smartparking*")
 public class SmartParkingApplication {
 
 	public static void main(String[] args) {

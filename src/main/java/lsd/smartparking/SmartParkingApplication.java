dc5cf4ee5cf0ed294a448b413b16e0d228977fd1
Luca Ruschioni <Ruschio@users.noreply.github.com>
2021-11-03 20:59:56 +0100

    Enable logger

--- src/main/java/lsd/smartparking/SmartParkingApplication.java
+++ src/main/java/lsd/smartparking/SmartParkingApplication.java
@@ -1,5 +1,6 @@
 package lsd.smartparking;
 
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class SmartParkingApplication {
 
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
 		SpringApplication.run(SmartParkingApplication.class, args);
 	}
 

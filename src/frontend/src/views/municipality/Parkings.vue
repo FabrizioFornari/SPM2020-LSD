3481a38216cd0378a08a9c504d28bd7c5ae95dca
Luca Ruschioni <Ruschio@users.noreply.github.com>
2021-11-09 19:56:51 +0100

    Add municipality frontend parkings' list

---
+++ src/frontend/src/views/municipality/Parkings.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="menu">
+        <div class="choices parkings">
+            <router-link class="choice parking" v-for="(parking, index) in parkings" :key="index" :to="'/map/parking/' + parking.id">
+                <h4>{{ parking.name }}</h4>
+                <p>{{ parking.address }}</p>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import apiParking from '@/api/parking'
+
+export default {
+    name: 'Parkings',
+    data() {
+        return {
+            parkings: []
+        }
+    },
+    async created() {
+        await apiParking.getParkings().then(response => {
+            this.parkings = response.data
+        })
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.menu {
+    .parking {
+        flex: 1 1 200px;
+    }
+}
+</style>
